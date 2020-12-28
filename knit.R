38de9c549fba8ab6b1e0dbe670ba580b9e1ed996
ignacymorawski <ignacy.morawski@spotdata.pl>
2020-12-28 13:20:06 +0000

    2020-12-28 13:20:06 - Update

--- knit.R
+++ knit.R
@@ -1,3 +1,5 @@
+Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio-server/bin/pandoc")
+print("tucośpowinnobyć")
 rmarkdown::render(input="/home/rstudio/PulsBiznesu/PulsBiznesu/index.Rmd", output_file="/home/rstudio/PulsBiznesu/PulsBiznesu/docs/index.html")
 UpdateGitHub()
 UpdateGitHub()
